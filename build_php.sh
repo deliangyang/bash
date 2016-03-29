6e3623d6f641550952c3fff0085866116d173a39
yangdeliang <yangdeliang@autohello.com>
2016-03-29 15:56:01 +0800

    update buid_php.sh

--- build_php.sh
+++ build_php.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 mkdir -p /root/source;
 
-TEST_MACHINE='root@xxx.com'
+TEST_MACHINE='root@xxxx.xxxx'
 BUILD_PATH='/usr/local/php'
 SOURCE_PATH='/root/kit'
-MY_SOURCE_PATH='/root/source/'
+MY_SOURCE_PATH='/root/source'
 
 function _color()
 {
@@ -52,9 +52,10 @@ tar -zvxf redis-2.2.7.tgz
 tar -zvxf php-5.6.19.tar.gz
 unzip mongo-php-driver-legacy-master.zip
 
-cd php-5.6.19
+cd $MY_SOURCE_PATH/php-5.6.19
 ./configure --enable-fpm --with-mysql --prefix=$BUILD_PATH
-make && make install
+make
+make install
 
 cp php.ini-development $BUILD_PATH/lib/php.ini
 cp $BUILD_PATH/etc/php-fpm.conf.default $BUILD_PATH/etc/php-fpm.conf
@@ -64,35 +65,36 @@ groupadd www-data >/dev/null 2>&1
 useradd -g www-data www-data >/dev/null 2>&1
 
 # 修改配置文件
-cat $BUILD_PATH/etc/php-fpm.conf | sed 's/^user = no-body$/user = www-data/' > php-fpm.conf
-cat $BUILD_PATH/etc/php-fpm.conf | sed 's/^group = no-body$/group = www-data/' > php-fpm.conf
+sed -i 's/^user = nobody$/user = www-data/' $BUILD_PATH/etc/php-fpm.conf
+sed -i 's/^group = nobody$/group = www-data/' $BUILD_PATH/etc/php-fpm.conf
 # 配置php-fpm pid & port
-cat $BUILD_PATH/etc/php-fpm.conf | sed 's/^;pid = run/php-fpm.pid$/pid = run/php-fpm.pid/' > php-fpm.conf
+sed -i 's/^;pid = run\/php-fpm.pid$/pid = run\/php-fpm.pid/' $BUILD_PATH/etc/php-fpm.conf
 # php-fpm 已经使用了, 修改端口
 if [ -n `netstat -apn | grep php-fpm` ]; then
-    cat $BUILD_PATH/etc/php-fpm.conf | sed 's/^listen = 127.0.0.1:9000$/listen = 127.0.0.1:9001/' > php-fpm.conf
+    sed -i 's/^listen = 127.0.0.1:9000$/listen = 127.0.0.1:9001/' $BUILD_PATH/etc/php-fpm.conf
 fi
 
 # php-fpm 自启动脚本
-cp php5-fpm.sh /ext/init.d/
-
-# 生成扩展路径
-EXTENSIONS=$BUILD_PATH/lib/php/extensions/no-debug-non-zts-`date +%Y%m%d`
+cp php5-fpm.sh /ext/init.d/php5-fpm
 
 mkdir -p $BUILD_PATH/ext
 
 # 安装扩展 REDIS / MONGO
-cd $SOURCE_PATH/redis-2.2.7
-`$BUILD_PATH/bin/phpize`
+echo "[+] 开始安装redis" | _color green
+cd $MY_SOURCE_PATH/redis-2.2.7
+$BUILD_PATH/bin/phpize
 ./configure --with-php-config=$BUILD_PATH/bin/php-config
-make && make install
-cp $EXTENSIONS/redis.so $BUILD_PATH/ext/redis.so
+make
+THE_EXTENSIONS=`make install | grep -P "$BUILD_PATH/lib/php/extensions/no-debug-non-zts-\d{8}" -o`
+cp $THE_EXTENSIONS/redis.so $BUILD_PATH/ext/redis.so
 
-cd $SOURCE_PATH/bin/mongo-php-driver-legacy-master
-`$BUILD_PATH/bin/phpize`
+echo "[+] 开始安装mongo" | _color green
+cd $MY_SOURCE_PATH/mongo-php-driver-legacy-master
+$BUILD_PATH/bin/phpize
 ./configure --with-php-config=$BUILD_PATH/bin/php-config
-make && make install
-cp $EXTENSIONS/mongo.so $BUILD_PATH/ext/mongo.so
+make
+THE_EXTENSIONS=`make install | grep -P "$BUILD_PATH/lib/php/extensions/no-debug-non-zts-\d{8}" -o`
+cp $THE_EXTENSIONS/mongo.so $BUILD_PATH/ext/mongo.so
 
 # 修改php.ini 添加扩展 (!-.- 直接复制测试机上面的php.ini)
 scp $TEST_MACHINE:/usr/local/php/lib/php.ini $BUILD_PATH/lib/php.ini
@@ -103,9 +105,4 @@ scp $TEST_MACHINE:/usr/local/php/lib/php.ini $BUILD_PATH/lib/php.ini
 
 echo '[+] 完成安装, php-fpm服务已启动' | _color green
 
-
-
-
-
-
-
+cd -
